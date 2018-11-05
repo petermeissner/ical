d3835ef53c59b1cd3fff2d3c059c019d905804c2
petermeissner <retep.meissner@gmail.com>
2018-11-05 12:06:15 +0100

    further tests

---
+++ NEWS.md
@@ -0,0 +1,17 @@
+# ical 0.1.2 -- 2018-11-05
+
+* Added a further tests
+* Added a `NEWS.md` file to track changes to the package.
+
+
+
+# ical 0.1.1 -- 2018-10-24
+
+* fix: ical_parse_df() would not return data.frame()
+
+
+
+# ical 0.1.0 -- 2018-10-11
+
+* setting things up
+* sketching things out
--- tests/testthat/test_ical_parse.R
+++ tests/testthat/test_ical_parse.R
@@ -16,10 +16,65 @@ test_that(
 
 test_that(
   desc = "ical_parse(file = ...)",
-    code =
-      expect_true({
-        ical_parse(file = system.file("birthdays.ics", package = "ical"))
+  code =
+    expect_true({
+      ical_parse(file = system.file("birthdays.ics", package = "ical"))
 
-        TRUE
-      })
+      TRUE
+    })
 )
+
+
+test_that(
+  desc = "ical_parse_df(text = ...)",
+  code =
+    expect_true({
+      tmp_df <-
+        ical_parse_df(
+          text = readLines(system.file("birthdays.ics", package = "ical"))
+        )
+
+      nrow(tmp_df) == 4
+    })
+)
+
+
+test_that(
+  desc = "ical_parse_df(file = ...)",
+  code =
+    expect_true({
+      tmp_df <-
+        ical_parse_df(file = system.file("birthdays.ics", package = "ical"))
+
+      nrow(tmp_df) == 4
+    })
+)
+
+
+
+
+test_that(
+  desc = "ical_parse_list(text = ...)",
+  code =
+    expect_true({
+      tmp_list <-
+        ical_parse_list(
+          text = readLines(system.file("birthdays.ics", package = "ical"))
+        )
+
+      length(tmp_list) & is.list(tmp_list)
+    })
+)
+
+
+test_that(
+  desc = "ical_parse_list(file = ...)",
+  code =
+    expect_true({
+      tmp_list <-
+        ical_parse_list(file = system.file("birthdays.ics", package = "ical"))
+
+      length(tmp_list) & is.list(tmp_list)
+    })
+)
+
